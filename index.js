652d5c2791659f6fc6537d59269a436445a9cd49
Rayngumi <rayngumi25@gmail.com>
2024-03-31 13:52:02 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+
+    let textParagraph = document.getElementById('text');
+  
+  
+    textParagraph.textContent = "This is really cool!";
+  });
